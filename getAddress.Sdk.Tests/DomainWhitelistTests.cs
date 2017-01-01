a5ba9e59d8cabf6504e3fd372a2dc97cea1717c3
Lee Smith <leepaulsmith@googlemail.com>
2017-01-01 16:38:10 +0000

    Tests

---
+++ getAddress.Sdk.Tests/DomainWhitelistTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+
+namespace getAddress.Sdk.Tests
+{
+    [TestClass]
+    public class DomainWhitelistTests
+    {
+
+        [TestMethod]
+        public async Task List()
+        {
+            var adminKey = KeyHelper.GetAdminKey();
+
+            using (var api = new GetAddesssApi(new AdminKey(adminKey)))
+            {
+                var result = await api.DomainWhitelist.List();
+
+                Assert.IsTrue(result.IsSuccess);
+            }
+             
+        }
+    }
+}
---
+++ getAddress.Sdk.Tests/KeyHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getAddress.Sdk.Tests
+{
+    static class KeyHelper
+    {
+        public static string GetAdminKey()
+        {
+            var adminKey = Environment.GetEnvironmentVariable("getaddress_adminkey", EnvironmentVariableTarget.User);
+
+            if (string.IsNullOrWhiteSpace(adminKey)) throw new Exception("Add your admin key to your Environmental Variables");
+
+            return adminKey;
+        }
+    }
+}
